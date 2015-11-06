e3f2d99a573af4f442b03066ee9f0582645a732e
Martin Schuster <Martin.Schuster1@infineon.com>
2015-11-06 09:39:18 +0100

    SRC: Make source pretty

--- ESP_BaPoTeSta.ino
+++ ESP_BaPoTeSta.ino
@@ -8,66 +8,72 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
+// configuration
 const char ssid[] = "tabr.org";
-WiFiUDP Udp;
 IPAddress myAddr(10, 1, 0, 35);
-IPAddress tempserver(10, 1, 0, 9);
-#define MEASURES 5
-#define PACKET_SIZE 5*MEASURES+2+11
-unsigned int sensorValue[MEASURES];
-#define SLEEPSEC 2
+IPAddress tempServer(10, 1, 0, 9);
+const unsigned int tempPort = 9988;
+const byte MEASURES = 5
+const byte SLEEPSEC = 2
+const byte BLUELED = 4;
 
+WiFiUDP Udp;
+unsigned int sensorValue[MEASURES];
 unsigned long int chipId;
 
-// the setup function runs once when you press reset or power the board
+
 void setup() {
-    // initialize digital pin 13 as an output.
-    // pinMode(4, OUTPUT);
-    // Serial.begin(115200);
-    // Serial.setDebugOutput(true);
+    // activate blue LED to show that we are "on"
+    pinMode(BLUELED, OUTPUT);
+    digitalWrite(BLUELED, HIGH);
+
+    // start WiFi
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid);
-    pinMode(4, OUTPUT);
-    digitalWrite(4, HIGH);   // turn the LED on (HIGH is the voltage level)
 
+    // wait a while for WiFi to associate and get IP
     delay(2000);
-    // long rssi = WiFi.RSSI();
-    // Serial.print("RSSI: ");
-    // Serial.println(rssi);
+
+    // get ChipID, will be used as unique ID when sending data
     chipId = ESP.getChipId();
 }
 
-// the loop function runs over and over again forever
+
+// this won't actually "loop", as the last command leads to a reset
 void loop() {
-    static byte led = 1;
 
-
+    // measure temp multiple times
     for (byte c=0; c<MEASURES; c++) {
         delay(100);
         sensorValue[c] = analogRead(A0);
     }
+
     delay(1000);
     sendTemp(sensorValue);
 
     // WAKE_RF_DEFAULT, WAKE_RFCAL, WAKE_NO_RFCAL, WAKE_RF_DISABLED
     ESP.deepSleep(10e6 * SLEEPSEC, WAKE_RF_DEFAULT);
-    //delay(1000);              // wait for a second
 }
 
+
 void sendTemp(unsigned int* temp) {
+    const byte PACKET_SIZE = 11 + 5*MEASURES + 1;
     static char packetBuffer[PACKET_SIZE];
 
     // set all bytes in the buffer to 0
     memset(packetBuffer, 0, PACKET_SIZE);
 
+    // start data with ChipID (so multiple stations can use the same server)
     sprintf(packetBuffer, "0x%08x ", chipId);
 
+    // add measured temperature values
     for (byte c=0; c<MEASURES; c++) {
         sprintf(11+packetBuffer+5*c, "%04d ", temp[c]);
     }
-    packetBuffer[11+5*MEASURES-1] = '\n';
+    // the buffer now ends with SPC NUL -- change SPC to Newline
+    packetBuffer[PACKET_SIZE - 2] = '\n';
 
-    Udp.beginPacket(tempserver, 9988);
+    Udp.beginPacket(tempServer, tempPort);
     Udp.write(packetBuffer, PACKET_SIZE);
     Udp.endPacket();
 }
