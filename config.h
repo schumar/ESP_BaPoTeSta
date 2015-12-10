973bbcc01a2be7fecfbda8335a65bf5ea6831bb4
Martin Schuster <Martin.Schuster1@infineon.com>
2015-12-10 22:09:09 +0100

    SRC: re-order config

--- config.h
+++ config.h
@@ -9,17 +9,19 @@ const IPAddress IPGateway(10, 1, 0, 1);
 const IPAddress IPSubnet(255, 255, 255, 0);
 const IPAddress IPServer(10, 1, 0, 9);
 const unsigned int portServer = 9988;
+const unsigned int maxPacketSize = 1400;
+// behaviour/timing
 const byte maxConnRetry = 200;   // in 50ms units!
 const unsigned int noConnSleepSec = 600;
-const unsigned int maxPacketSize = 1400;
+const unsigned int SLEEPSEC = 307; // not very accurate
 // hardware
 const byte PIN_BLUELED = 1;
 const byte BLUELED_ON = LOW;  // onboard-LED is active-LOW
+
+// NTC
 const byte PIN_NTC = 5;
 const unsigned int NTC_ID = 42;
 const byte NTC_MEASURES = 5;
-// behaviour
-const unsigned int SLEEPSEC = 307; // not very accurate
 // temp calculation
 const float Vdd = 3.3;      // voltage of PIN_NTC when HIGH
 const float Voff = -0.01;   // highest V where ADC still reports "0"
