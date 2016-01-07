89585449cbf126b8f43bf1578181e9e51aa81861
Martin Schuster <github@isja.org>
2016-01-07 19:59:27 +0100

    BUG: Used non-defined PIN_XXX_POWER
    those are consts, not defines, so the code never got compiled

--- ESP_BaPoTeSta.ino
+++ ESP_BaPoTeSta.ino
@@ -229,17 +229,13 @@ void powerNTC(bool on) {
 }
 
 void powerDallas(bool on) {
-#ifdef PIN_DALLAS_POWER
     pinMode(PIN_DALLAS_POWER, OUTPUT);
     digitalWrite(PIN_DALLAS_POWER, on ? HIGH : LOW);
-#endif
 }
 
 void powerDHT(bool on) {
-#ifdef PIN_DHT_POWER
     pinMode(PIN_DHT_POWER, OUTPUT);
     digitalWrite(PIN_DHT_POWER, on ? HIGH : LOW);
-#endif
 }
 
 
